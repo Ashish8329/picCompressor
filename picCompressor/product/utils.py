fe1855439be164de1b3d348595ad034f5f3b0efc
Ashish Auti <ashishauti123@gmail.com>
2025-03-10 11:27:56 +0530

    feat(csv Validation) : add csv data validation

--- picCompressor/product/utils.py
+++ picCompressor/product/utils.py
@@ -1,10 +1,22 @@
 from rest_framework import serializers
+import csv
+import io
 
 def validate_csv_file(csv_file):
-        """Validates the uploaded CSV file"""
-        try:
-            if not csv_file.name.endswith(".csv"):
-                raise ValueError("Uploaded file must be a CSV.")
+    """Validates the uploaded CSV file"""
+    try:
+        if not csv_file.name.endswith(".csv"):
+            raise ValueError("Uploaded file must be a CSV.")
+        
+        # Read and decode file (Django's InMemoryUploadedFile or TemporaryUploadedFile)
+        decoded_file = csv_file.read().decode('utf-8')
+        file_io = io.StringIO(decoded_file)  # Convert string to file-like object
 
-        except ValueError as e:
-            raise serializers.ValidationError({"csv_file": str(e)})
+        reader = csv.reader(file_io)
+        header = next(reader, None)  # Read the first row (header)
+        
+        if header is None or len(header) != 2:
+            raise ValueError("Error: handling CSV data, make sure the CSV data is in the provided format.")
+
+    except (ValueError, UnicodeDecodeError) as e:
+        raise serializers.ValidationError({"csv_file": str(e)})
