fcde9306a1dc6736c0c7baa6739417e695bb7689
Ashish Auti <ashishauti123@gmail.com>
2025-03-11 12:12:01 +0530

    feat(api) : add the get api with req param

--- picCompressor/product/serializers.py
+++ picCompressor/product/serializers.py
@@ -1,14 +1,23 @@
 from rest_framework import serializers
-from .models import Product, Image
+
+from .models import Image, Product
 from .utils import validate_csv_file
+
 
 class ProductSerializer(serializers.ModelSerializer):
     csv_file = serializers.FileField(required=True)
+
     class Meta:
         model = Product
-        fields = '__all__'
-    
+        fields = "__all__"
+
     def validate(self, attrs):
-        csv_file = attrs.get('csv_file')
+        csv_file = attrs.get("csv_file")
         validate_csv_file(csv_file)
         return super().validate(attrs)
+
+
+class ImageSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Image
+        fields = "__all__"
--- picCompressor/product/views.py
+++ picCompressor/product/views.py
@@ -1,11 +1,14 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 from rest_framework import status, viewsets
 from rest_framework.response import Response
 
+from base.choices import Status
+
 from .models import Image, Product
-from .serializers import ProductSerializer
+from .serializers import ImageSerializers, ProductSerializer
 from .utils import process_images
 
 
@@ -14,10 +17,71 @@ class ProdcutViewSet(viewsets.ModelViewSet):
     serializer_class = ProductSerializer
 
     def list(self, request, *args, **kwargs):
+        """
+        Retrieve and process images associated with a given request ID.
 
-        return super().list(request, *args, **kwargs)
+        """
+        req_id = request.GET.get("req_id")
+
+        # Validate if req_id is provided
+        if not req_id:
+            return Response(
+                {"error": "Missing required parameter: req_id"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        try:
+            images = Image.objects.filter(product__req_id=req_id).values_list(
+                "status", "compressed_image"
+            )
+
+            if not images.exists():
+                return Response(
+                    {"error": "No images found for the given request ID"},
+                    status=status.HTTP_404_NOT_FOUND,
+                )
+
+            total_img_count = len(images)
+            compred_img_count = sum(
+                1 for status, _ in images if status == Status.COMPLETED.value[0]
+            )
+            compressed_images_urls = [img_url for _, img_url in images]
+
+            compressed_perc = (
+                round((compred_img_count / total_img_count) * 100)
+                if total_img_count
+                else 0
+            )
+
+            response_data = {
+                "request_id": req_id,
+                "status": Status.PENDING.value[0],
+            }
+
+            if compressed_perc == 100:
+                response_data["output_image_urls"] = compressed_images_urls
+                response_data["status"] = Status.COMPLETED.value[0]
+
+            else:
+                response_data["compressed_Perc"] = compressed_perc
+
+            return Response(response_data)
+
+        except ObjectDoesNotExist:
+            return Response(
+                {"error": "Invalid request ID or associated product does not exist"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        except Exception as e:
+            return Response(
+                {"error": f"An unexpected error occurred: {str(e)}"},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            )
+
 
     def create(self, request, *args, **kwargs):
+
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         product = serializer.save()
