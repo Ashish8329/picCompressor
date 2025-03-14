ad6f9f38eef97d7fc0190fe8ca5d764bbfce6569
Ashish Auti <ashishauti123@gmail.com>
2025-03-14 15:12:19 +0530

    feat : add button component

---
+++ front_end/picCompressor_FE/src/components/Button.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './button.css'
+
+const Button = () => {
+  return (
+    <div className="container-fluid">
+                <div className="row">
+                    <div id="ms-container">
+                        <label htmlFor="ms-download">
+                            <div className="ms-content">
+                                <div className="ms-content-inside">
+                                    <input type="checkbox" id="ms-download" />
+                                    <div className="ms-line-down-container">
+                                        <div className="ms-line-down" />
+                                    </div>
+                                    <div className="ms-line-point" />
+                                </div>
+                            </div>
+                        </label>
+                    </div>
+                </div>
+            </div>
+  )
+}
+
+export default Button
