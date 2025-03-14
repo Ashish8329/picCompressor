e7b1ee265cabb1040a66c62d2a2a9044432d91eb
Ashish Auti <ashishauti123@gmail.com>
2025-03-14 15:11:56 +0530

    feat : added button component

--- front_end/picCompressor_FE/src/App.jsx
+++ front_end/picCompressor_FE/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
+import Button from './components/Button'
 
 function App() {
   const [count, setCount] = useState(0)
